e9bb9f144af8530540aee05c6bdd4855118926b2
caretapp <app.caret@gmail.com>
2023-02-27 11:33:41 -0500

    updated a few cards

--- app/src/main/java/com/please/caretonboarding/cardAdapter.java
+++ app/src/main/java/com/please/caretonboarding/cardAdapter.java
@@ -14,6 +14,7 @@ import android.provider.Settings;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.widget.AppCompatImageView;
@@ -87,6 +88,7 @@ public class cardAdapter extends PagerAdapter {
 						ClipboardManager clipboard = (ClipboardManager) context.getSystemService(Context.CLIPBOARD_SERVICE);
 						ClipData clip = ClipData.newPlainText("id", keyText.getText().toString());
 						clipboard.setPrimaryClip(clip);
+						Toast.makeText( context, "Key copied to clipboard!", Toast.LENGTH_SHORT ).show();
 					}
 				});
 				View venmoButton = viewList[position].findViewById(R.id.venmo_view);
